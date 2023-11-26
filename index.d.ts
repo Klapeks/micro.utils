4a22eb607fb81b3ab9fc8b1a29dc988e64c784cb
Klapeks <sashaKaraguts@gmail.com>
2023-11-26 04:54:47 +0200

    1.5.7 - random + default routes

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 import AuthTokens, { SelfUser as ISelfUser } from "./dist/auth.tokens";
 import parseDatabasePath from "./dist/data/dbpath.parser";
 import { HttpException, HttpStatus, NotAuthException } from "./dist/express-utils/exceptions";
-import MRouter from "./dist/express-utils/mrouter";
+import MRouter, { DefaultRoutes } from "./dist/express-utils/mrouter";
 import globalEnv from "./dist/global.env";
 import MicroServer, { MicroAxios } from "./dist/micro.server";
 import utils, { assertNever, bits } from './dist/utils';
 export type SelfUser = ISelfUser;
-export { MRouter, HttpStatus, HttpException, NotAuthException, AuthTokens, MicroServer, MicroAxios, globalEnv, parseDatabasePath, utils, bits, assertNever };
+export { MRouter, DefaultRoutes, HttpStatus, HttpException, NotAuthException, AuthTokens, MicroServer, MicroAxios, globalEnv, parseDatabasePath, utils, bits, assertNever };
