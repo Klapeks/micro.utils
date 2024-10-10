130801eb050fe7f00d843baf2e4850569f39bec6
Klapeks <sashaKaraguts@gmail.com>
2024-10-10 21:59:17 +0300

    Update micro.axios.ts

--- src/micro/micro.axios.ts
+++ src/micro/micro.axios.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 import globalEnv from "../global.env";
-import { logger } from "@klapeks/utils";
+import { HttpException, logger } from "@klapeks/utils";
 
 type _AI = AxiosInstance;
 export type MicroAxios = AxiosInstance & {
@@ -10,6 +10,14 @@ export type MicroAxios = AxiosInstance & {
     deleteData<T = any>(...params: Parameters<_AI['delete']>): Promise<T>,
 }
 
+function is502err(err: any): any {
+    if (err?.response?.status === 502) {
+        err = err.request.path || err.config.url;
+        if (!err) throw new HttpException("Bad Gateway", 502);
+        throw new HttpException("Can't access " + err, 502);
+    }
+    throw err;
+}
 export function createMicroAxios(uri?: string) {
     if (!uri) uri = globalEnv.servers.api;
     logger.debug("Creating big axios instance", uri);
@@ -27,16 +35,16 @@ export function createMicroAxios(uri?: string) {
     }) as any as MicroAxios;
 
     api.getData = async function (...params: Parameters<_AI['get']>) {
-        return (await api.get(...params)).data;
+        return (await api.get(...params).catch(is502err)).data;
     };
     api.postData = async function (...params: Parameters<_AI['post']>) {
-        return (await api.post(...params)).data;
+        return (await api.post(...params).catch(is502err)).data;
     };
     api.deleteData = async function (...params: Parameters<_AI['delete']>) {
-        return (await api.delete(...params)).data;
+        return (await api.delete(...params).catch(is502err)).data;
     };
     api.patchData = async function (...params: Parameters<_AI['patch']>) {
-        return (await api.patch(...params)).data;
+        return (await api.patch(...params).catch(is502err)).data;
     };
     
     return api;
