e510392cb8ee0ce6cf085545da8c13c0ec87ea79
Klapeks <sashaKaraguts@gmail.com>
2024-10-14 00:06:54 +0300

    beta8 - after.init update

--- src/express/after.init.ts
+++ src/express/after.init.ts
@@ -1,5 +1,6 @@
 import { catchRouterError } from "@klapeks/utils";
 import { IRouter } from "express";
+import globalEnv from "../global.env";
 
 export default function afterInit(app: IRouter, showErrors: boolean) {
     return () => {
@@ -10,10 +11,11 @@ export default function afterInit(app: IRouter, showErrors: boolean) {
         });
         app.use((req, res, next) => {
             res.status(404).send({
+                status: 404,
                 error: "API not found",
                 'api-path': req.url,
                 method: req.method,
-                status: 404
+                micro: globalEnv.serverOptions.microServer,
             })
         });
     }
